76b23a80a1d6f57ed10ca5307f94588b116840e3
Sergey Rubanov <chi187@gmail.com>
2023-09-25 22:51:28 +0200

    feat: use Vite as a template generator

--- templates/tonic/test/index.js
+++
@@ -1,7 +0,0 @@
-import { test } from 'socket:test'
-import os from 'socket:os'
-
-test('test', async t => {
-  const label1 = document.querySelector('h1').textContent
-  t.equal(label1, `Hello, ${os.platform()}`, 'label on start is correct')
-})
